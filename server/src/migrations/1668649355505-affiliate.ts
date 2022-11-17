a81b8548b16439789035e8a512f7746f9d955369
Jaqueline Malinoski <95326101+jaquemalinoski@users.noreply.github.com>
2022-11-16 23:39:06 -0300

    feat: config heroku

---
+++ server/src/migrations/1668649355505-affiliate.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class affiliate1668649355505 implements MigrationInterface {
+    name = 'affiliate1668649355505'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "affiliates" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "type" character varying NOT NULL, "date" character varying NOT NULL, "product" character varying NOT NULL, "amount" character varying NOT NULL, "seller" character varying NOT NULL, CONSTRAINT "PK_5458bf988fb83086da3a14b9ff9" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "affiliates"`);
+    }
+
+}
